74820c366cadf297879d6f86348b00c960dde23e
prostolyalya <prostolyalya@outlook.com>
2020-07-01 10:50:00 +0300

    [Work] add convert

--- lib.cpp
+++ lib.cpp
@@ -3,3 +3,11 @@
 Lib::Lib()
 {
 }
+
+std::string Lib::convert(const int &num)
+{
+    std::stringstream stream;
+    stream << std::hex << num;
+    std::string result( stream.str() );
+    return result;
+}
--- lib.h
+++ lib.h
@@ -1,10 +1,16 @@
 #ifndef LIB_H
 #define LIB_H
 
+#include <string>
+#include <sstream>
+
 class Lib
 {
 public:
     Lib();
+
+public:
+    static std::string convert(const int & num);
 };
 
 #endif // LIB_H
