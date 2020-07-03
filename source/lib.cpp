d992c2e95a0dad9b0e11491badae50c4535df75f
prostolyalya <prostolyalya@outlook.com>
2020-07-03 14:46:19 +0300

    folders

---
+++ source/lib.cpp
@@ -0,0 +1,18 @@
+#include "header/lib.h"
+
+std::string libConvert::convertDecToHex(const int &num)
+{
+    std::stringstream stream;
+    stream << std::hex << num;
+    std::string result( stream.str() );
+    return result;
+}
+
+int libConvert::convertHexToDec(const std::string &data)
+{
+    std::stringstream stream;
+    stream << std::hex << data;
+    int res = 0;
+    stream >> res;
+    return res;
+}
